141ebd2f61e285b0a80d3961b15c11a63efb520a
Ryan Emerson <emersonryant@gmail.com>
2021-11-18 15:05:29 +0200

    conversation screen structure

--- lib/screens/conversation_screen.dart
+++ lib/screens/conversation_screen.dart
@@ -8,10 +8,46 @@ class ConversationScreen extends StatefulWidget {
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
+  // Widget ChatMessageList() {
+
+  // }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Conversation"),
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Colors.grey[300],
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      // controller: searchTextEditingController,
+                      decoration: InputDecoration(
+                          hintText: "Type a message...",
+                          hintStyle: TextStyle(color: Colors.grey[400]),
+                          border: InputBorder.none),
+                    )),
+                    GestureDetector(
+                      child: Icon(
+                        Icons.send,
+                        size: 30,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
